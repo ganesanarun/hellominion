c7b82362503818bae74f83e93b14bc89350b9402
ganesan.arunachalam <ganesan1063@gmail.com>
2018-04-07 12:28:46 +0530

    Add proc file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var http = require('http');
+const PORT = process.env.PORT || 5000
 
 var server = http.createServer(function(request, response) {
     response.writeHead(200);
     response.end("hell world");
 });
 
-server.listen(8000);
+server.listen(PORT);
